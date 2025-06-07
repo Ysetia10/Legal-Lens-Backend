f444ba70a5eead92295b03767dcf9d32dc2f06a6
Yashan Setia <yashansetia7@gmail.com>
2025-06-07 16:33:18 +0530

    intial commit

---
+++ src/main/java/com/legalLens/LegalLensApplication.java
@@ -0,0 +1,11 @@
+package com.legalLens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalLensApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LegalLensApplication.class, args);
+    }
+}
